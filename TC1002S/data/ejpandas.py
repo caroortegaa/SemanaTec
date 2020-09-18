b30ea05d994bb77fd3f21d9db2594f33fa2f260e
Caro Ortega <caroortega@MacBook-de-Caro-2.local>
2020-09-17 20:48:25 -0500

    caro

--- TC1002S/data/ejpandas.py
+++ TC1002S/data/ejpandas.py
@@ -1,26 +1,24 @@
 import pandas as pd
 
-def read_pandas(filepath):
+def read_pandas(filepath , name):
     """
     read a csv in pandas
     """
 
-iris_df = pd.read_csv(
-    filepath,
-    names=['sepal_length','sepal_width','petal_length','petal_width','class'])
+df = pd.read_csv(filepath,
+names=['sepal_length','sepal_width','petal_length','petal_width','class'])
 
-iris_df
-
+print(df)
 
 with open(filepath,'r') as fp:
     data = fp.read().split('\n')
 
 data = [f.split(',') for f in data]
 
-iris_df_v2 = pd.DataFrame(
+df2 = pd.DataFrame(
     data=data,
     columns=['sepal_length','sepal_width','petal_length','petal_width','class'])
-     
+
 if __name__ == "__main__":
     filepath = "./data/iris.data"
     df =read_pandas(filepath)
---
+++ TC1002S/proyecto.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+# 1
+
+list1 = [1,2,3,4,5]
+list2 = [5,4,3,2,1]
+
+def distance(list1, list2):
+    if len(list1) != len(list2):
+        return -1
+    d_squared = 0
+    for v1, v2 in zip(list1, list2):
+        d_squared += (v2 - v1)**2
+
+    return d_squared**(1/2)
+
+# 2
+
+puntos = [[2,2], [4,4], [5,5], [14,14], [16,16]]
+centros = [[3,3], [15,15]]
+
+def cercanos(puntos, centros):
+    clusters = [[] for _ in range(0, len(centros))]
+    for i, punto in enumerate(puntos):
+        p_vs_c = []
+        for i, centro in enumerate(centros):
+            d = distance(centro, punto)
+            p_vs_c.append(d)
+        pos = p_vs_c.index(min(p_vs_c))
+        clusters[pos].append(punto)
+
+    return clusters
+
+
+# 3
+
+def centers(clusters):
+    new_centers = []
+    for cluster in clusters:
+        c = [sum(x) for x in zip(*cluster)]
+        for i, _ in enumerate(c):
+            c[i] /= len(cluster)
+        new_centers.append(c)
+    return new_centers
+
+print(centers(cercanos(puntos, centros)))
+
+
+#4
+
+def k_means(puntos):
+    i=0
+    while i < 100:
+        centr = np.random.randint(puntos)
+        k = clusters(centr)
+        new_centers = centr(k)
+    return new_centers
+    i+1
+print (new_centers , k)
